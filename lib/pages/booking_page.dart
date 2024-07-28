d19eee7ad7bf67963109062640ae0cc24dc35e3e
RyanMikhael <ryanmikhael20@gmail.com>
2024-07-28 19:37:06 -0300

    feat: changing BookingPage

--- lib/pages/booking_page.dart
+++ lib/pages/booking_page.dart
@@ -11,6 +11,8 @@ class BookingPage extends StatefulWidget {
 
 class _BookingPageState extends State<BookingPage> {
 
+  bool select = false;
+
   
   @override
   Widget build(BuildContext context) {
@@ -24,26 +26,72 @@ class _BookingPageState extends State<BookingPage> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20,),
-            const Center(child: Text('Agende seu serviço', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),),
-            const SizedBox(height: 16,),
-            Container(decoration: BoxDecoration(border:Border.all(color:Colors.black),borderRadius: const BorderRadius.all(Radius.circular(8))),child: CalendarDatePicker(initialDate: DateTime(2024,07, 25), firstDate: DateTime(2024,1), lastDate: DateTime(2025,12), onDateChanged: (value){})),
-            const SizedBox(height: 20,),
-            GestureDetector(
-              onTap: (){
-              },
-              child: const CircleAvatar(
-                radius: 50,
-                child: Image(width: 90, height: 90, image: AssetImage('images/Barbeiro.png',),),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // const SizedBox(height: 20,),
+              const Center(child: Text('Agende seu serviço', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),),
+              const SizedBox(height: 16,),
+              Container(decoration: BoxDecoration(border:Border.all(color:Colors.black),borderRadius: const BorderRadius.all(Radius.circular(8))),child: CalendarDatePicker(initialDate: DateTime(2024,07, 25), firstDate: DateTime(2024,1), lastDate: DateTime(2025,12), onDateChanged: (value){})),
+              const SizedBox(height: 20,),
+              GestureDetector(
+                onTap: (){
+                },
+                child: const CircleAvatar(
+                  radius: 50,
+                  child: Image(width: 90, height: 90, image: AssetImage('images/Barbeiro.png',),),
+                )
+                ),
+              const Text('Nome do barbeiro',style: TextStyle(fontWeight: FontWeight.bold),),
+              const SizedBox(height: 20,),
+              const Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  ChoiceChip(label: Text('08:30'), selected: false,),
+                  ChoiceChip(label: Text('09:15'), selected: false),
+                  ChoiceChip(label: Text('10:00'), selected: false),
+                  ChoiceChip(label: Text('10:45'), selected: false),
+                  ChoiceChip(label: Text('11:30'), selected: false),
+                  ChoiceChip(label: Text('12:15'), selected: false),
+        
+                ],
+              ),
+              const SizedBox(height: 30,),
+              
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: const Color.fromARGB(59, 59, 59, 1)
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [Text('Nome do serviço', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),), Text('RS 50', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),)],),
+                      SizedBox(height: 8,),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [Text('Data marcada'), Text('10 de junho')],),
+                      SizedBox(height: 8,),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Text('Barbeiro'), Text('Nome do barbeiro')],),
+                      SizedBox(height: 8,),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Text('Horário'), Text('09:30')],)
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12,),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                child: ElevatedButton(style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8), side: const BorderSide(color: Colors.black))
+                ),onPressed: (){}, child: const Text('Confirmar agendamento', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),)),
               )
-              )
-          ],
-        )
+            ],
+          )
+        ),
       )
     );
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:barberapp/pages/booking_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,7 +19,47 @@ class HomePage extends StatelessWidget {
         
       ),
       drawer: Drawer(
-        child: ListView(children: [],),
+        child: ListView(children: const [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: CircleAvatar(child:Icon(Icons.person)),
+              ),
+              ListTile(title: Text('Nome do usuário'), subtitle: Text('emaildousuario@gmail.com'),)
+            ],
+          ),
+          Divider(
+            height: 1,
+            thickness: 1,
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Página inicial'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Minha conta'),
+          ),
+          ListTile(
+            leading: Icon(Icons.check),
+            title: Text('Agendamentos'),
+          ),
+          Divider(
+            height: 1,
+            thickness: 1,
+          ),
+          ListTile(
+            leading: Icon(Icons.question_mark),
+            title: Text('Sobre'),
+          ), 
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Sair da conta'),
+          )
+
+        ],),
       ),
 
       body: Padding(
@@ -42,6 +80,7 @@ class HomePage extends StatelessWidget {
             const SizedBox(height: 20,),
 
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Card(
                   child: Column(
@@ -58,7 +97,6 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(width: 20,),
                 Card(
                   child: Column(
                     children: [
@@ -74,7 +112,6 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(width: 20,),
                 Card(
                   child: Column(
                     children: [
@@ -96,23 +133,28 @@ class HomePage extends StatelessWidget {
             const Text('Todos os serviços', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
             const SizedBox(height: 20,),
 
-            Container(
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                color:  Color.fromRGBO(59, 59, 59, 1.0),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-              width: MediaQuery.of(context).size.width,
-              height: 110,
-              child: ListTile(
-                onTap: ()  async {
-                  await Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const BookingPage() ));
-                },
-                leading: const Image(image: AssetImage('images/service.png'),),
-                title: const Text('Nome do serviço',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),),
-                subtitle: const Text('Informações do serviço', style: TextStyle(fontSize: 14, color: Colors.white),),
-                trailing: const Text("RS 50", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),),
-
+            Card(
+              elevation: 8,
+              color: const Color.fromRGBO(59, 59, 59, 1.0),
+              shape: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+              child: Container(
+                alignment: Alignment.center,
+                // decoration: const BoxDecoration(
+                //   color:  Color.fromRGBO(59, 59, 59, 1.0),
+                //   borderRadius: BorderRadius.all(Radius.circular(8))
+                // ),
+                width: MediaQuery.of(context).size.width,
+                height: 110,
+                child: ListTile(
+                  onTap: ()  async {
+                    await Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const BookingPage() ));
+                  },
+                  leading: const Image(image: AssetImage('images/service.png'),),
+                  title: const Text('Nome do serviço',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),),
+                  subtitle: const Text('Informações do serviço', style: TextStyle(fontSize: 14, color: Colors.white),),
+                  trailing: const Text("RS 50", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),),
+              
+                ),
               ),
             )
           ],
